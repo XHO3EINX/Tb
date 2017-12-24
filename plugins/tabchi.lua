804e04e3b8747bc6c6739d271716793fac0c92a4
XHO3EINX <0xshayeax0@gmail.com>
2017-12-24 12:51:42 +0330

    Update tabchi.lua

--- plugins/tabchi.lua
+++ plugins/tabchi.lua
@@ -1,5 +1,5 @@
 -- Start TabchiBot
-local sudomsg = 282958812 -- put your id here 
+local sudomsg = 300131107 -- put your id here 
 local function reload_plugins( )
   plugins = {}
   load_plugins()
